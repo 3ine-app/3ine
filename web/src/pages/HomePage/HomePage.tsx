e02f236a3f70768d9fe4dae32c34136a198bfedc
Brian Liu <brian@vouchfor.com>
2021-08-16 22:29:24 +1000

    Init commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <div>
+      <img
+        src="/android-chrome-192x192.png"
+        loading="lazy"
+        alt="3ine"
+        width="192"
+        height="192"
+      />
+    </div>
+  )
+}
+
+export default HomePage
