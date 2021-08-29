dc41c55976244efe243cd2297cbd90a07fe46edd
Brian Liu <brian@vouchfor.com>
2021-08-29 14:15:57 +1000

    Tweaked about page

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,25 @@
-import { Link, NavLink, routes } from '@redwoodjs/router'
+import { Link, NavLink, routes, useMatch } from '@redwoodjs/router'
+import { FC } from 'react'
 import { FaTwitter } from 'react-icons/fa'
+import { FiMenu } from 'react-icons/fi'
+import { IoMdClose } from 'react-icons/io'
 
 type MainLayoutProps = {
   children?: React.ReactNode
+}
+
+const CustomNavLink: FC<{ to: string }> = ({ to, children }) => {
+  const { match } = useMatch(to)
+
+  return (
+    <li>
+      <Link to={to}>
+        <span className={match ? 'border-b-2 border-primary-focus' : ''}>
+          {children}
+        </span>
+      </Link>
+    </li>
+  )
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
@@ -10,82 +27,128 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   const brandName = document
     .getElementsByTagName('meta')
     .namedItem('brandName').content
+  const logo = (
+    <div className="flex items-center">
+      <img
+        width="60"
+        height="60"
+        loading="lazy"
+        alt="3ine Footer Logo"
+        src="images/icon-192x192.png"
+      />
+      <div className="ml-3 text-xl font-extrabold font-gruppo text-shadow">
+        {brandName}
+      </div>
+    </div>
+  )
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="shadow-md sticky top-0 bg-base-100 z-10 font-gruppo">
-        <div className="flex items-center max-w-7xl mx-auto h-14 px-6 xl:px-0">
-          <Link
-            to={routes.home()}
-            className="text-4xl font-extrabold navbar-logo"
-          >
-            <span className="inline-block">3</span>
-            <span>ine</span>
-          </Link>
-          <div className="flex-1 text-right">
+    <div className="drawer">
+      <input id="menu-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="flex flex-col min-h-screen drawer-content">
+        <div className="shadow-md sticky top-0 bg-base-100 z-10 font-gruppo">
+          <div className="flex items-center max-w-7xl mx-auto h-14 pl-6 pr-4 xl:px-0">
             <Link
-              className="btn btn-sm btn-ghost mr-2 text-base"
               to={routes.home()}
+              className="text-4xl font-extrabold navbar-logo text-shadow"
             >
-              <span className="font-serif">W</span>
-              <span className="lowercase">hy</span>
+              <span className="inline-block">3</span>
+              <span>ine</span>
             </Link>
-            <Link
-              className="btn btn-sm btn-ghost mr-2 text-base"
-              to={routes.home()}
-            >
-              <span className="font-serif">W</span>
-              <span className="lowercase">here</span>
-            </Link>
-            <Link
-              className="btn btn-sm btn-ghost mr-2 text-base"
-              to={routes.home()}
-            >
-              <span className="font-serif">W</span>
-              <span className="lowercase">hen</span>
-            </Link>
-            <NavLink
-              className="btn btn-sm btn-ghost mr-2 text-base"
-              activeClassName="bg-neutral text-white hover:bg-neutral"
-              to={routes.about()}
-            >
-              <span className="capitalize">About</span>
-            </NavLink>
-          </div>
-        </div>
-      </div>
-      <main className="flex-1 max-w-7xl mx-auto mt-20 px-6 xl:px-0">
-        {children}
-      </main>
-      <footer className="p-10 footer flex items-center flex-col-reverse sm:flex-row bg-gradient-to-t from-neutral to-base-100">
-        <div className="flex-1 place-items-center sm:place-items-start">
-          <div className="flex items-center">
-            <img
-              width="60"
-              height="60"
-              loading="lazy"
-              alt="3ine Footer Logo"
-              src="images/icon-192x192.png"
-            />
-            <div className="ml-3 text-xl font-extrabold font-gruppo">
-              {brandName}
+            <div className="hidden flex-1 text-right sm:block">
+              <Link
+                className="btn btn-sm btn-ghost mr-2 text-base"
+                to={routes.home()}
+              >
+                <span className="font-serif">W</span>
+                <span className="lowercase">hy</span>
+              </Link>
+              <Link
+                className="btn btn-sm btn-ghost mr-2 text-base"
+                to={routes.home()}
+              >
+                <span className="font-serif">W</span>
+                <span className="lowercase">here</span>
+              </Link>
+              <Link
+                className="btn btn-sm btn-ghost mr-2 text-base"
+                to={routes.home()}
+              >
+                <span className="font-serif">W</span>
+                <span className="lowercase">hen</span>
+              </Link>
+              <NavLink
+                className="btn btn-sm btn-ghost mr-2 text-base"
+                activeClassName="bg-neutral text-white hover:bg-neutral"
+                to={routes.about()}
+              >
+                <span className="capitalize">About</span>
+              </NavLink>
+            </div>
+            <div className="flex-1 text-right sm:hidden">
+              <label htmlFor="menu-drawer" className="btn btn-square btn-ghost">
+                <FiMenu size={28} />
+              </label>
             </div>
           </div>
-          <p>Copyright © {current.getFullYear()} - All right reserved</p>
         </div>
-        <div>
-          <div className="grid grid-flow-col gap-4">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              referrerPolicy="no-referrer"
-              href="https://twitter.com/ralphbliu"
-            >
-              <FaTwitter size="28" />
-            </a>
+        <main className="flex-1 max-w-7xl mx-auto mt-14 sm:mt-20 px-6 xl:px-0">
+          {children}
+        </main>
+        <footer className="p-10 footer flex items-center flex-col-reverse sm:flex-row bg-gradient-to-t from-neutral to-base-100">
+          <div className="flex-1 place-items-center sm:place-items-start">
+            {logo}
+            <p className="font-medium text-shadow-light">
+              Copyright © {current.getFullYear()} - All right reserved
+            </p>
           </div>
-        </div>
-      </footer>
+          <div>
+            <div className="grid grid-flow-col gap-4">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                referrerPolicy="no-referrer"
+                href="https://twitter.com/ralphbliu"
+              >
+                <FaTwitter size="28" className="drop-shadow-lg" />
+              </a>
+            </div>
+          </div>
+        </footer>
+      </div>
+      <div className="drawer-side font-gruppo">
+        <label htmlFor="menu-drawer" className="drawer-overlay">
+          &nbsp;
+        </label>
+        <ul className="p-4 overflow-y-auto menu w-80 bg-base-100">
+          <li className="pt-2 pb-4">
+            <div className="flex items-center">
+              <div className="flex-1">{logo}</div>
+              <label
+                htmlFor="menu-drawer"
+                className="btn btn-square btn-ghost "
+              >
+                <IoMdClose size={30} />
+              </label>
+            </div>
+          </li>
+          <CustomNavLink to={routes.home()}>
+            <span className="font-serif">W</span>
+            <span className="lowercase">hy</span>
+          </CustomNavLink>
+          <CustomNavLink to={routes.home()}>
+            <span className="font-serif">W</span>
+            <span className="lowercase">here</span>
+          </CustomNavLink>
+          <CustomNavLink to={routes.home()}>
+            <span className="font-serif">W</span>
+            <span className="lowercase">hen</span>
+          </CustomNavLink>
+          <CustomNavLink to={routes.about()}>
+            <span className="capitalize">About</span>
+          </CustomNavLink>
+        </ul>
+      </div>
     </div>
   )
 }
--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -15,45 +15,59 @@ const AboutPage = () => {
           <div>Wine</div>
         </div>
       </div>
-      <div className="text-lg">
-        Wine is beautiful but could be a bit intimidated to get close to it. We
-        are a group of wine enthusiasts deems to share our knowledge for{' '}
-        <mark className="bg-primary text-primary-content">free</mark> in all
-        sorts of vehicles to help everyone start enjoying wine without paying a
-        dime and hassles. We summarized{' '}
-        <span className="text-primary-focus font-serif font-extrabold">3</span>{' '}
-        most important topics of everything about{' '}
-        <span className="text-primary-focus font-serif font-extrabold">w</span>
-        ine you would need to focus on, thus{' '}
-        <span className="text-primary-focus font-serif font-extrabold">
-          3ine
-        </span>{' '}
-        is born.
+      <div className="lg:mx-32 text-justify">
+        <div className="mb-4">
+          <span className="text-shadow font-semibold font-serif text-xl">
+            Wine
+          </span>{' '}
+          or{' '}
+          <span className="text-shadow font-semibold font-serif text-xl">
+            3 wine
+          </span>
+          ? Whatever you would like to call it, we provide quintessential
+          knowledge of wine in{' '}
+          <span className="text-shadow font-semibold font-serif text-xl">
+            3 Ws
+          </span>{' '}
+          for <span className="text-shadow font-semibold font-serif">free</span>
+          . Yes, everything you want or need to know about wine to help you
+          becoming savvy to pick good bottles for your lovely night with
+          significant one or friends at a fancy restaurant or event paving your
+          way to become professional in wine.
+        </div>
+        <div>
+          Wine is beautiful and everyone is deserved to appreciate it without
+          costing you fortune. Getting understand about wine could be daunting
+          and quite often you will need to pay to start with. We are a group of
+          wine enthusiasts who are holding different levels of WSET certificates
+          and devoted to share our passion and knowledge to people for free and
+          bring more people into the wine world.
+        </div>
       </div>
-      <div className="flex flex-col lg:flex-row mt-10">
+      <div className="flex flex-col lg:flex-row mt-20">
         {[
           {
             title: 'hy',
             icon: <GiGrapes size={40} />,
             description:
-              'Learn basic knowledge about grapes, regions, terroir, colour, countries, history, wine making, wine cellaring and even WSET courses, all free.',
+              'Why wine tasted dry? why steak with full-bodied wine? why different glasses? why temperature matters? why aging process changes taste? Any questions you have about wine, we cover for you, no buzz words, no charges, all free.',
           },
           {
             title: 'here',
             icon: <FaWineBottle size={40} />,
             description:
-              'Learn basic knowledge about grapes, regions, terroir, colour, countries, history, wine making, wine cellaring and even WSET courses, all free.',
+              'Buying wine can be frustrated especially searching bargains for specific wine region, grape, winery (château) or vintage. There are so many wine brokers out there selling different wines in different prices, you never get all the wines you want from the same broker in the cheapest price among them, thus we do it all for you.',
           },
           {
             title: 'hen',
             icon: <RiCalendarEventLine size={40} />,
             description:
-              'Learn basic knowledge about grapes, regions, terroir, colour, countries, history, wine making, wine cellaring and even WSET courses, all free.',
+              'Do you know Pinot Palooza? Do you know Taste Champagne? Do you know Good Food and Wine Show? We collects all the wine events for you based on your current location, so you will not miss any.',
           },
         ].map(({ icon, title, description }, index) => (
           <div
             key={`about-${index}w`}
-            className={`card shadow-2xl lg:card-side bg-neutral text-neutral-content ${
+            className={`card flex-1 shadow-2xl lg:card-side bg-gradient-to-tl from-neutral to-neutral-focus text-neutral-content ${
               index <= 2 ? 'mb-10 lg:mr-10' : ''
             }`}
           >
@@ -65,7 +79,7 @@ const AboutPage = () => {
                 </div>
                 <span>{icon}</span>
               </div>
-              <p className="font-extralight">{description}</p>
+              <p className="font-extralight text-justify">{description}</p>
             </div>
           </div>
         ))}
